3971bea852934a14bceb5684446d3ce02a3ac6bb
dom <wagnr@icloud.com>
2018-12-11 19:19:37 +0100

    Taking Jose's solution

--- Day5.exs
+++ Day5.exs
@@ -1,12 +1,14 @@
 #! /usr/bin/env elixir
 
 defmodule Day5 do
+  def react_polymer(input) when is_binary(input), do: react_polymer(input, [])
 
-  def react_polymer(input) do
-      input
-      |> Enum.chunk_every(10)
-  end
+  defp react_polymer(<<b, rest::binary>>, [a | acc]) when abs(a - b) == 32,
+    do: react_polymer(rest, acc)
 
+  defp react_polymer(<<b, rest::binary>>, acc), do: react_polymer(rest, [b | acc])
+
+  defp react_polymer(<<>>, acc), do: acc |> Enum.reverse() |> List.to_string()
 end
 
 case System.argv() do
@@ -18,32 +20,21 @@ case System.argv() do
 
       import Day5
 
-      def string_to_stream(input) do
-        {:ok, io} = StringIO.open(input)
-        IO.stream(io, 10)
-      end
-
-      def test_stream do
-        string_to_stream("""
-dabAcCaCBAcCcaDA
-""")
-      end
-
       test "Part 1" do
-        assert react_polymer(test_stream()) == "dabCBAcaDA"
+        assert react_polymer("dabAcCaCBAcCcaDA") == "dabCBAcaDA"
       end
 
-#      test "Part 2" do
-#        assert strategy2_guard_times_minute(test_stream()) == 99 * 45
-#      end
+      #      test "Part 2" do
+      #        assert strategy2_guard_times_minute(test_stream()) == 99 * 45
+      #      end
     end
 
   [input_file] ->
     input_file
-    |> File.stream!([],10)
+    |> File.read!()
     |> Day5.react_polymer()
+    |> byte_size()
     |> IO.inspect(label: "Result Part 1")
-
 
   _ ->
     IO.puts(:stderr, "Usage: #{Path.basename(__ENV__.file)} [--test | filename]")
