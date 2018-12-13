e84eebd5df9b170121e4e0cd099974baf37ace51
Dominik Wagner <wagnr@icloud.com>
2018-12-13 16:10:02 +0100

    Solved Day 9 Part II by implementing a more efficient list for that purpose

--- day9/lib/day9.ex
+++ day9/lib/day9.ex
@@ -1,16 +1,117 @@
+defmodule EList do
+  defstruct list: [],
+            length: 0
+
+  def new(list, length) do
+    %EList{
+      list: list,
+      length: length
+    }
+  end
+
+  def new(list) do
+    %EList{
+      list: list,
+      length: length(list)
+    }
+  end
+
+  def pop_at(%EList{list: list} = elist, location) when is_list(list) do
+    {entry, list} = List.pop_at(elist.list, location)
+
+    {
+      entry,
+      %EList{
+        list: list,
+        length: elist.length - 1
+      }
+    }
+  end
+
+  def pop_at(%EList{list: {elists}} = elist, location) do
+    {new_elists, {entry}} =
+      elists
+      |> Enum.reduce({[], location}, fn
+        el, {acc, {entry}} ->
+          {[el | acc], {entry}}
+
+        el, {acc, loc} ->
+          if loc < el.length do
+            {entry, new_el} = EList.pop_at(el, loc)
+            {[new_el | acc], {entry}}
+          else
+            {[el | acc], loc - el.length}
+          end
+      end)
+
+    {
+      entry,
+      %EList{
+        list: { Enum.reverse(new_elists) },
+        length: elist.length - 1
+      }
+    }
+  end
+
+  def insert_at(%EList{list: list} = elist, location, value) when is_list(list) do
+    if elist.length == 100 do
+      {l1, l2} = Enum.split(list, 50)
+
+      EList.insert_at(
+        %EList{
+          list:
+            {[
+               new(l1, 50),
+               new(l2, 50)
+             ]},
+          length: elist.length
+        },
+        location,
+        value
+      )
+    else
+      %EList{
+        list: List.insert_at(elist.list, location, value),
+        length: elist.length + 1
+      }
+    end
+  end
+
+  def insert_at(%EList{list: {elists}} = elist, location, value) do
+    new_elists =
+        elists
+        |> Enum.reduce({[], location}, fn
+          el, {acc, loc} ->
+            if loc <= el.length do
+              [EList.insert_at(el, loc, value) | acc]
+            else
+              {[el | acc], loc - el.length}
+            end
+
+          el, acc when is_list(acc) ->
+            [el | acc]
+        end)
+        |> Enum.reverse()
+
+    %EList{
+      list: {new_elists},
+      length: elist.length + 1
+    }
+  end
+end
+
 defmodule Day9 do
-  defstruct scores: [],
+  defstruct scores: %{},
             current: 0,
-            marbles: [0],
+            marbles: EList.new([0]),
             player_count: 0,
             last_played_marble: 0
 
   def new(player_count) do
-    %Day9{scores: 1..player_count |> Enum.map(fn _ -> [] end), player_count: player_count}
+    %Day9{player_count: player_count}
   end
 
   def to_string() do
-    
   end
 
   @moduledoc """
@@ -36,56 +137,65 @@ defmodule Day9 do
         iex> Day9.marble_game(30,5807)
         37305
 
+        iex> Day9.marble_game(30,5807 * 5)
+        822794
+
         iex> Day9.marble_game(465,71498)
         383475
+
+        iex> Day9.marble_game(465,71498 * 100)
+        3148209772
+
   """
   def marble_game(player_count, last_marble) do
     initial = new(player_count)
-#    IO.puts("#{initial}")
-    
+    #    IO.puts("#{initial}")
+
     1..last_marble
     |> Enum.reduce(
       initial,
       &marble_game_step/2
     )
-#    |> (fn state -> IO.puts("#{state}")
-#      state
-#    end).()
-    |> IO.inspect(label: "End")
+    #    |> (fn state -> IO.puts("#{state}")
+    #      state
+    #    end).()
+    #    |> IO.inspect(label: "End")
     |> case do
-        %Day9{scores: scores} -> Enum.map(scores, fn scorelist -> Enum.sum(scorelist) end)
-      end
+      %Day9{scores: scores} -> Enum.map(scores, fn {_player, score} -> score end)
+    end
     |> Enum.max()
   end
 
-  def marble_game_step(marble, %Day9{current: current} = state) do
+  def marble_game_step(marble, state) do
     state = %Day9{state | last_played_marble: marble}
 
     case rem(marble, 23) do
       0 ->
         player = rem(marble - 1, state.player_count)
-        current = rem(state.current + length(state.marbles) - 7, length(state.marbles))
-        {taken, marbles} = List.pop_at(state.marbles, current)
+        current = rem(state.current + state.marbles.length - 7, state.marbles.length)
+        {taken, marbles} = EList.pop_at(state.marbles, current)
 
         %Day9{
           state
-          | scores: List.update_at(state.scores, player, fn t -> [taken, marble | t] end),
+          | scores:
+              Map.update(state.scores, player, taken + marble, fn o -> o + taken + marble end),
             current: current,
             marbles: marbles
         }
 
       _ ->
-        current = rem(state.current + 1, length(state.marbles)) + 1
+        current = rem(state.current + 1, state.marbles.length) + 1
 
         %Day9{
           state
           | current: current,
-            marbles: List.insert_at(state.marbles, current, marble)
+            marbles: EList.insert_at(state.marbles, current, marble)
         }
     end
-#    |> (fn state -> IO.puts("#{state}")
-#      state
-#    end).()
+
+    #    |> (fn state -> IO.puts("#{state}")
+    #      state
+    #    end).()
   end
 end
 
@@ -93,8 +203,9 @@ defimpl String.Chars, for: Day9 do
   def to_string(state) do
     marble = state.last_played_marble
     current = state.current
-    
+
     display_player = rem(marble - 1, state.player_count)
+
     IO.iodata_to_binary([
       "[#{
         if display_player >= 0 do
@@ -113,8 +224,5 @@ defimpl String.Chars, for: Day9 do
         {m, _} -> [" ", m, " "]
       end)
     ])
-    
   end
 end
-
-
