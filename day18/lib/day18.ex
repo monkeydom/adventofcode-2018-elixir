fbcea3de331cca4618d7d8dcea06d405ab9a1bc2
Dominik Wagner <wagnr@icloud.com>
2018-12-18 10:51:59 +0100

    More start of work for Day 18

--- day18/lib/day18.ex
+++ day18/lib/day18.ex
@@ -1,5 +1,53 @@
 defmodule Lumber do
+  defstruct field: [],
+    generation: 0,
+    dimension: 0
+    
+  def new(string) when is_binary(string) do
+    field = 
+    string
+    |> String.split("\n", trim: true)
+    |> Enum.map(&pad_line/1)
+    |> Enum.map(&String.to_charlist/1)
+    |> pad_field()
+    
+    length = length(hd(field))
+
+    %__MODULE__{
+      field: field,
+      generation: 0,
+      dimension: length - 2
+    }
+  end
   
+  defp padding(n), do: padding(n, []) 
+  defp padding(0, acc), do: acc
+  defp padding(n, acc), do: padding(n-1, [?. | acc])
+
+  defp pad_line(line), do: <<".", line::binary, ".">>
+  defp pad_field(field) do  
+    length = length(hd(field))
+    empty = padding(length)
+    
+    [empty | 
+      [empty | Enum.reverse(field)] 
+      |>Enum.reverse()]
+  end 
+  
+  def step(%__MODULE__{field: field, generation: generation, dimension: dimension}) do
+    field = step(field, [])
+
+    %__MODULE__{
+      field: field,
+      generation: generation+1,
+      dimension: dimension
+    }
+  end
+  
+  defp step([[]], acc), do: acc
+  defp step(field, acc) do
+    field
+  end   
 end
 
 defmodule Day18 do
@@ -21,7 +69,6 @@ defmodule Day18 do
       :world
 """
   def lumber(input) do
-    input
-    |> String.split("\n")
+    Lumber.new(input)
   end
 end
