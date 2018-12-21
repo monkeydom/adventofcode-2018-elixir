5e9d3f06fd7497dd33bfa9357297896ef279c0d2
Dominik Wagner <wagnr@icloud.com>
2018-12-21 22:40:53 +0100

    Added all tests for Day20 Part I and added a solution script file

--- day20/lib/day20.ex
+++ day20/lib/day20.ex
@@ -14,9 +14,18 @@ defmodule Day20 do
       iex> Day20.part1("^ENWWW(NEEE|SSE(EE|N))$")
       10
 
+      iex> Day20.part1("^ENWWW(NEEE|SSEEE|SSEN)$")
+      10
+
       iex> Day20.part1("^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$")
       18
 
+
+      iex> Day20.part1("^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$")
+      23
+      
+      iex> Day20.part1("^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$")
+      31
   """
   def part1(<<?^, path::binary>>) do
     pos = {0,0}
@@ -26,7 +35,11 @@ defmodule Day20 do
     |> elem(1)
   end
   
-  defp collect_paths("$", _current, distance_map, []), do: distance_map
+  defp collect_paths("$", {pos, distance}, distance_map, []) do
+    # IO.write(<<"\u001b[5D">>)
+    # IO.write(String.pad_leading("#{distance}", 5, " "))
+    distance_map
+  end
 
   defp collect_paths(<<h, tail::binary>>, {pos, distance}, distance_map, stack) when h in [?N, ?E, ?S, ?W] do
     next_pos = next_pos(pos, h)
@@ -45,7 +58,10 @@ defmodule Day20 do
   end
 
   defp collect_paths(<<")", tail::binary>>, current, distance_map, [{{_prev_pos, _prev_distance}, branches} | stacktail]) do
+    # branch_length = length(branches)+1
+    #IO.write(String.pad_leading("", branch_length, "1234567890"))
     [current | branches]
+    |> Enum.uniq()
     |> Enum.reduce(distance_map, fn {pos, distance}, distance_map ->
       collect_paths(tail, {pos, distance}, distance_map, stacktail)
     end)
